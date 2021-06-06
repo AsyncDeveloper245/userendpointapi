57cbac91849480e3b349096c9a2888bc7e45a3a8
AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>
2021-06-06 15:41:42 +0100

    change whitenoise settings

--- userdata/views.py
+++ userdata/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from rest_framework.generics import RetrieveAPIView,ListAPIView
+from rest_framework.generics import RetrieveAPIView,ListCreateAPIView
 from .serializers import CustomUserSerializer
 from django.contrib.auth import get_user_model
 
 
 # Create your views here.
-class CustomUserListAPIView(ListAPIView):
+class CustomUserListAPIView(ListCreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = CustomUserSerializer
 
